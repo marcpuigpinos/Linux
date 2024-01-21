6601895543d042bf6e135ba891a0102bed252034
Marc Puigpinos <9293767+marcpuigpinos@users.noreply.github.com>
2024-01-21 16:15:03 +0100

    Update 01_configure_fedora_sudo.sh

--- 01_configure_fedora_sudo.sh
+++ 01_configure_fedora_sudo.sh
@@ -10,6 +10,7 @@ dnf install gfortran -y
 #dnf update -y
 #dnf install akmod-nvidia -y 
 #dnf install xorg-x11-drv-nvidia-cuda -y 
+dnf install ffmpeg --allowerasing -y
 dnf install rofi -y
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
