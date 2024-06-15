dc4e4af26c8a3c9ea2955e24bc8fd83b3dd32898
Marc Puigpinos <marc.puigpinos@gmail.com>
2024-06-15 13:30:47 +0200

    config_1 updated

--- fedora-i3-config1/fish/config.fish
+++ fedora-i3-config1/fish/config.fish
@@ -1,4 +1,4 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/takuya.omp.json | source
+    # Commands to run in interactive sessions can go here
+    oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/zash.omp.json | source
 end
