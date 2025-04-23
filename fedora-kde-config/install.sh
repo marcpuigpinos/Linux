fb6723a813e779f9a57313db22e1fd86b8ea04e6
MarcPB <marc.puigpinos@gmail.com>
2025-04-23 15:25:34 +0200

    Update neovim configuration to use lazyvim

--- fedora-kde-config/install.sh
+++ fedora-kde-config/install.sh
@@ -2,89 +2,89 @@
 
 # Function to install Nvidia drivers
 install_nvidia() {
-    # Add commands to install Nvidia drivers here
-    sudo dnf install akmod-nvidia -y 
-    #sudo dnf install xorg-x11-drv-nvidia-cuda -y 
+  # Add commands to install Nvidia drivers here
+  sudo dnf install akmod-nvidia -y
+  #sudo dnf install xorg-x11-drv-nvidia-cuda -y
 }
 
 # Function to install AMD drivers
 install_amd() {
-    # Add commands to install AMD drivers here
-    echo "Not implemented yet"
+  # Add commands to install AMD drivers here
+  echo "Not implemented yet"
 }
 
 # Function to display the help message
 display_help() {
-    echo "Usage: $0 [options]"
-    echo
-    echo "Options:"
-    echo "  --install-graphic-card nvidia    Install Nvidia graphic card drivers"
-    echo "  --install-graphic-card amd       Install AMD graphic card drivers"
-    echo "  --help                           Display this help message"
-    echo
-    echo "Example:"
-    echo "  $0 --install-graphic-card nvidia"
-    echo "  $0 --install-graphic-card amd"
-    echo "  $0"
+  echo "Usage: $0 [options]"
+  echo
+  echo "Options:"
+  echo "  --install-graphic-card nvidia    Install Nvidia graphic card drivers"
+  echo "  --install-graphic-card amd       Install AMD graphic card drivers"
+  echo "  --help                           Display this help message"
+  echo
+  echo "Example:"
+  echo "  $0 --install-graphic-card nvidia"
+  echo "  $0 --install-graphic-card amd"
+  echo "  $0"
 }
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --install-graphic-card)
-            if [[ -n "$2" && "$2" != "--"* ]]; then
-                GRAPHIC_CARD="$2"
-                shift
-            else
-                echo "Error: --install-graphic-card requires an argument: 'nvidia' or 'amd'"
-                exit 1
-            fi
-            ;;
-        --help)
-            display_help
-            exit 0
-            ;;
-        *)
-            echo "Unknown option: $1"
-            display_help
-            exit 1
-            ;;
-    esac
-    shift
+  case $1 in
+  --install-graphic-card)
+    if [[ -n "$2" && "$2" != "--"* ]]; then
+      GRAPHIC_CARD="$2"
+      shift
+    else
+      echo "Error: --install-graphic-card requires an argument: 'nvidia' or 'amd'"
+      exit 1
+    fi
+    ;;
+  --help)
+    display_help
+    exit 0
+    ;;
+  *)
+    echo "Unknown option: $1"
+    display_help
+    exit 1
+    ;;
+  esac
+  shift
 done
 
 # Main script logic
 if [[ "$GRAPHIC_CARD" == "nvidia" ]]; then
-    echo "Installing Nvidia drivers..."
+  echo "Installing Nvidia drivers..."
 elif [[ "$GRAPHIC_CARD" == "amd" ]]; then
-    echo "Installing AMD drivers..."
+  echo "Installing AMD drivers..."
 elif [[ -z "$GRAPHIC_CARD" ]]; then
-    echo "No graphic card drivers will be installed."
+  echo "No graphic card drivers will be installed."
 else
-    echo "Invalid option for --install-graphic-card: $GRAPHIC_CARD. Valid options are 'nvidia' or 'amd'."
-    display_help
-    exit 1
+  echo "Invalid option for --install-graphic-card: $GRAPHIC_CARD. Valid options are 'nvidia' or 'amd'."
+  display_help
+  exit 1
 fi
 
 echo "You might be asked for password more than one time..."
 sudo dnf update -y && sudo dnf upgrade -y && sudo dnf clean all -y
-sudo dnf install gcc -y 
-sudo dnf install g++ -y 
+sudo dnf install gcc -y
+sudo dnf install g++ -y
 sudo dnf install python-pip -y
 sudo dnf install gfortran -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf update -y
 if [[ "$GRAPHIC_CARD" == "nvidia" ]]; then
-    install_nvidia
+  install_nvidia
 elif [[ "$GRAPHIC_CARD" == "amd" ]]; then
-    install_amd
+  install_amd
 elif [[ -z "$GRAPHIC_CARD" ]]; then
-    echo "No graphic card drivers will be installed."
+  echo "No graphic card drivers will be installed."
 fi
 sudo dnf install neovim -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
 sudo dnf check-update -y
 sudo dnf install code -y
 sudo dnf install git -y
@@ -109,7 +109,7 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/mast
 # Copy configuration files
 cp .bashrc ~/.bashrc
 mkdir -p ~/.config/nvim
-cp .config/nvim/init.lua ~/.config/nvim 
+cp .config/nvim/* ~/.config/nvim
 
 echo "Installation finished!"
 
